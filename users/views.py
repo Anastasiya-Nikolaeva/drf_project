a7d875bea6e6338d07ab51ba5a4954a527f59a25
Anastasiya Nikolaeva <eritreya666@gmail.com>
2025-04-03 22:28:21 +0300

    update project

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django_filters import rest_framework as filters
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
 from materials.models import Course, Lesson
-
 from .models import Payment, User
 from .serializers import PaymentSerializer, UserSerializer
 
@@ -16,7 +16,6 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
 
 
 class PaymentFilter(filters.FilterSet):
-    payment_date = filters.DateFilter(field_name="payment_date", lookup_expr="exact")
     paid_course = filters.ModelChoiceFilter(queryset=Course.objects.all())
     paid_lesson = filters.ModelChoiceFilter(queryset=Lesson.objects.all())
     payment_method = filters.ChoiceFilter(
@@ -25,13 +24,13 @@ class PaymentFilter(filters.FilterSet):
 
     class Meta:
         model = Payment
-        fields = ["payment_date", "paid_course", "paid_lesson", "payment_method"]
+        fields = ["paid_course", "paid_lesson", "payment_method"]
 
 
 class PaymentListView(generics.ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
     filterset_class = PaymentFilter
     ordering_fields = ["payment_date"]
     ordering = ["payment_date"]
