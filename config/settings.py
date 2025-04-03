2a96681ae9bfb72b5534c2654ec847e41f1cf482
Anastasiya Nikolaeva <eritreya666@gmail.com>
2025-04-03 22:34:38 +0300

    update project

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,12 @@ INSTALLED_APPS = [
     "materials",
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
