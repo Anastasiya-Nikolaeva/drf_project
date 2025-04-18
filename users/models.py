582d5428b7f9a1b3a4dbc52e350b8b01c9045874
Anastasiya Nikolaeva <eritreya666@gmail.com>
2025-04-18 12:11:52 +0300

    update project

--- users/models.py
+++ users/models.py
@@ -1,13 +1,29 @@
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 from materials.models import Course, Lesson
 
 
-class User(AbstractUser):
-    username = None
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, default="default_username")
     email = models.EmailField(
         unique=True, verbose_name="Почта", help_text="Укажите почту"
     )
@@ -32,9 +48,12 @@ class User(AbstractUser):
         verbose_name="Фото",
         help_text="Загрузите фотографию",
     )
+    is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
 
 class Payment(models.Model):
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+class UserModelTests(TestCase):
+
+    def test_create_user(self):
+        user = User.objects.create_user(email="test@example.com", password="testpass")
+        self.assertEqual(user.email, "test@example.com")
+        self.assertTrue(user.check_password("testpass"))
+
+    def test_create_superuser(self):
+        superuser = User.objects.create_superuser(
+            email="admin@example.com", password="adminpass"
+        )
+        self.assertEqual(superuser.email, "admin@example.com")
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_superuser)
+
+    def test_user_str(self):
+        user = User.objects.create_user(email="test@example.com", password="testpass")
+        self.assertEqual(str(user), "test@example.com")
